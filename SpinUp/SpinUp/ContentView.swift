d9dd68d40faac07a12d74aa58fee77e66f345144
kongthn <kongthn@gmail.com>
2022-06-08 11:38:52 +0900

    change project to SwiftUI

---
+++ SpinUp/SpinUp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SpinUp
+//
+//  Created by GOngTAE on 2022/06/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SpinUp/SpinUp/SpinUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpinUpApp.swift
+//  SpinUp
+//
+//  Created by GOngTAE on 2022/06/08.
+//
+
+import SwiftUI
+
+@main
+struct SpinUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
